9c92b36eb054be4b642ca166fc1e2aca09701fa4
Peter <petermeng1997@gmail.com>
2023-03-13 10:12:35 -0700

    first commit

---
+++ Assets/_Project/Networking/NewsAPI.cs
@@ -0,0 +1,102 @@
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NewsAPI : MonoBehaviour
+{
+
+    private string URL = "http://localhost:3000/news/getScripts?limit=10&skip=0";
+    private PlayerData playerData;
+    private string path = "";
+    private string persistentPath = "";
+    public Text Response;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // SetPaths();
+        // StartCoroutine(SetPaths());
+        StartCoroutine(GetData());
+        
+        //CreatePlayerData();
+        //SaveData();
+    }
+
+    private void CreatePlayerData()
+    {
+        playerData = new PlayerData("Nico", 200f, 10f, 3);
+    }
+
+    private void SetPaths()
+    {
+        path = Application.dataPath + Path.AltDirectorySeparatorChar + "_Project/Data/" + "NewsData.json";
+        persistentPath = Application.persistentDataPath + Path.AltDirectorySeparatorChar + "_Project/Data/" + "NewsData.json";
+    }
+
+   public void SaveData()
+    {
+        string savePath = path;
+
+        Debug.Log("Saving Data at " + savePath);
+        string json = JsonUtility.ToJson(playerData);
+        Debug.Log(json);
+
+        using StreamWriter writer = new StreamWriter(savePath);
+        writer.Write(json);
+    }
+
+    public void LoadData()
+    {
+        using StreamReader reader = new StreamReader(persistentPath);
+        string json = reader.ReadToEnd();
+
+        PlayerData data = JsonUtility.FromJson<PlayerData>(json);
+        Debug.Log(data.ToString());
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.S))
+    //         SaveData();
+
+    //     if (Input.GetKeyDown(KeyCode.L))
+    //         LoadData();
+    // }
+
+    IEnumerator GetData()
+    {
+
+        path = Application.dataPath + Path.AltDirectorySeparatorChar + "_Project/Data/" + "NewsData.json";
+        persistentPath = Application.persistentDataPath + Path.AltDirectorySeparatorChar + "_Project/Data/" + "NewsData.json";
+
+        using(UnityWebRequest request= UnityWebRequest.Get(URL))
+        {
+            yield return request.SendWebRequest();
+
+            if(request.result==UnityWebRequest.Result.ConnectionError)
+                Debug.LogError(request.error);
+            else
+            {
+                string json = request.downloadHandler.text;
+                SimpleJSON.JSONNode information = SimpleJSON.JSON.Parse(json);
+
+                Debug.Log("News Response: " + json);
+
+                string savePath = path;
+
+                Debug.Log("Saving Data at " + savePath);
+                //string json = JsonUtility.ToJson(playerData);
+                Debug.Log(information);
+
+                // using StreamWriter writer = new StreamWriter(information);
+                // writer.Write(information);
+
+                Response.text = information;
+            }
+        }
+    }
+}
